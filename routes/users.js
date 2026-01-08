c1e7af934cad1e37dc07c1a43447335eed55e24c
wuessslima <lim24012@byui.edu>
2026-01-08 14:30:41 -0300

    finishing the lesson

---
+++ routes/users.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const userController = require("../controllers/users");
+
+router.get("/", userController.getAll);
+router.get("/:id", userController.getSingle);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+const mongodb = require("./data/database");
 
 const port = process.env.PORT || 3000;
 
-app.use('/', require('./routes'));
+app.use("/", require("./routes"));
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(`Database is listening and Node running on port ${port}`);
+    });
+  }
 });
